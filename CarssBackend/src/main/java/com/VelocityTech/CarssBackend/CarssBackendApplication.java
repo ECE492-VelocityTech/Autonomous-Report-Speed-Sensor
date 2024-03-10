8de94438db14d1172fedece04d1722436df79adc
unknown <jsjugal81@gmail.com>
2024-03-09 19:31:50 -0700

    reverting ini's .env changes

--- CarssBackend/src/main/java/com/VelocityTech/CarssBackend/CarssBackendApplication.java
+++ CarssBackend/src/main/java/com/VelocityTech/CarssBackend/CarssBackendApplication.java
@@ -1,6 +1,5 @@
 package com.VelocityTech.CarssBackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,9 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class CarssBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.out.println("GOOGLE_API_KEY: " + dotenv.get("GOOGLE_API_KEY"));
-
 		SpringApplication.run(CarssBackendApplication.class, args);
 	}
 
