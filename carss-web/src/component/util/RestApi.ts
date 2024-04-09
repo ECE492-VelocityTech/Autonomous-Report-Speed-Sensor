e42525cec84ef1ca896c65ea435e9c1816ae4217
Meharpreet Singh Nanda <msnanda515@gmail.com>
2024-04-09 12:13:04 -0600

    Add backend url to env file

--- carss-web/src/component/util/RestApi.ts
+++ carss-web/src/component/util/RestApi.ts
@@ -1,5 +1,6 @@
-export const ServerUrl = "http://localhost:8080";
-// export const ServerUrl = "http://129.128.215.79/backend/api/v1";
+// export const ServerUrl = "http://localhost:8080";
+// export const ServerUrl = "http://129.128.215.79/backend";
+export const ServerUrl = process?.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
 
 const RestApi = {
 }
